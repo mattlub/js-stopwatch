a8073f44cc8de0b3ac43b6a6c94835f317e3364b
Matt Lubel <matt_lubel@hotmail.com>
2016-11-11 18:18:22 +0000

    added lap functionality

--- script.js
+++ script.js
@@ -3,13 +3,23 @@
 var start_button = document.getElementById("start-button");
 var stop_button = document.getElementById("stop-button");
 var reset_button = document.getElementById("reset-button");
+var lap_button = document.getElementById("lap-button");
 // var start_button_text = document.getElementById("start-stop-button-text");
 // var reset_button_text = document.getElementById("reset-button-text");
-var INTERVAL = 5;
+var INTERVAL = 10;
+
 var time_display = document.getElementById("time-display");
+var curr_lap_display = document.getElementById("curr-lap-display");
+var lap_times_display = document.getElementById("lap-times-display");
+
+var laps = [],
+	most_recent_lap,
+	lapHTML;
+	
 var interval_id,
 	start_time,
-	display_time = 0;
+	display_time = 0,
+	curr_lap_time = 0;
 
 var format_time = function(t) {
 	var cs = Math.floor((t/10)%100);
@@ -30,7 +40,9 @@ var format_time = function(t) {
 
 var update_display = function() {
 	time_display.innerHTML = format_time(display_time);
+	curr_lap_display.innerHTML = format_time(curr_lap_time);
 	display_time += INTERVAL;
+	curr_lap_time += INTERVAL;
 };
 
 var start = function() {
@@ -49,9 +61,41 @@ var stop = function() {
 
 var reset = function() {
 	display_time = 0;
+	curr_lap_time = 0;
 	update_display();
+	laps = [];
+	clear_laps();
+};
+
+var clear_laps = function() {
+	lap_times_display.innerHTML = ""
+}
+
+var write_lap = function(l) {
+	if (l.length == 1) {	
+		most_recent_lap = l[0];
+	}
+	else {
+		most_recent_lap = l[l.length - 1] - l[l.length - 2];
+	}
+	lapHTML = "<div id='lap'>Lap " + l.length +": " + format_time(most_recent_lap) +"</div>"
+	lap_times_display.innerHTML += lapHTML
+}
+
+var lap = function() {
+	// TODO- implement laps when clock stopped?
+	if (interval_id) {
+	// if clock running
+		laps.push(display_time);
+		write_lap(laps);
+		curr_lap_time = 0;
+	}
 };
 
 start_button.addEventListener("click", start);
 stop_button.addEventListener("click", stop);
 reset_button.addEventListener("click", reset);
+lap_button.addEventListener("click", lap);
+
+
+
