71fa575c2d28c30a6aa803600ccf4ac4aa52372b
Matt Lubel <matt_lubel@hotmail.com>
2016-11-15 10:31:36 +0000

    styled and changed tabs to spaces

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # js Stopwatch
--------------------
-js Stopwatch is a basic javascript stopwatch I created. Try it out!
 
-##### Motivations
--------------------
+js Stopwatch is a basic javascript stopwatch I created.
+Try it out [here](https://mattlub.github.io/js-stopwatch/)
+
+
+### Motivations
 I wanted to practise using vanilla js to make make a responsive app, which required some not-completely-elemental
 business logic. The result is quite good but also a little messy, as I wasn't thinking much about structure 
 and modulising(?) at the time.
 
+
 ##### Planned Improvements
 -------------------
-- add lap button (DONE)
-- style it up
-- make it a more modular app
-- maybe combine buttons, e.g. start/stop, lap/reset
-- export lap times to csv
-- mobile friendly/mobile first
-- add tests 
+- [x] add lap button (DONE)
+- [x] style it up (DONE initially 15/11/16)
+- [] make it a more modular app
+- [] maybe combine buttons, e.g. start/stop, lap/reset
+- [] re-structure bottom half of watch to have buttons and lap times side by side
+- [] export lap times to csv
+- [] mobile friendly/mobile first
+- [] add tests 
 
 ##### Collaboration
 -------------------
-Any collaboration/comments are more than welcome
+Any collaboration/comments are more than welcome.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//lots of global variables- sorry!
+// TODO- make it modular
 
 var start_button = document.getElementById("start-button");
 var stop_button = document.getElementById("stop-button");
@@ -13,83 +13,83 @@ var curr_lap_display = document.getElementById("curr-lap-display");
 var lap_times_display = document.getElementById("lap-times-display");
 
 var laps = [],
-	most_recent_lap,
-	lapHTML;
-	
+    most_recent_lap,
+    lapHTML;
+    
 var interval_id,
-	start_time,
-	display_time = 0,
-	curr_lap_time = 0;
+    start_time,
+    display_time = 0,
+    curr_lap_time = 0;
 
 var format_time = function(t) {
-	var cs = Math.floor((t/10)%100);
-	if (cs < 10) {
-		cs = "0" + cs;
-	}
-	var s = Math.floor((t/1000) % 60)  ;
-	if (s < 10) {
-		s = "0" + s;
-	}
-	var m = Math.floor((t/60000) % 60);
-	if (m < 10) {
-		m = "0" + m;
-	}
-	return m + ":" + s + ":" + cs;
+    var cs = Math.floor((t/10)%100);
+    if (cs < 10) {
+        cs = "0" + cs;
+    }
+    var s = Math.floor((t/1000) % 60)  ;
+    if (s < 10) {
+        s = "0" + s;
+    }
+    var m = Math.floor((t/60000) % 60);
+    if (m < 10) {
+        m = "0" + m;
+    }
+    return m + ":" + s + ":" + cs;
 };
 
 
 var update_display = function() {
-	time_display.innerHTML = format_time(display_time);
-	curr_lap_display.innerHTML = format_time(curr_lap_time);
-	display_time += INTERVAL;
-	curr_lap_time += INTERVAL;
+    time_display.innerHTML = format_time(display_time);
+    curr_lap_display.innerHTML = format_time(curr_lap_time);
+    display_time += INTERVAL;
+    curr_lap_time += INTERVAL;
 };
 
 var start = function() {
-	// to stop multiple starts
-	if (!interval_id) {
-		interval_id = window.setInterval(update_display, INTERVAL);
-	} else {
-		// maybe button glow red or something.
-	};
+    // to stop multiple starts
+    if (!interval_id) {
+        interval_id = window.setInterval(update_display, INTERVAL);
+    } else {
+        // maybe button glow red or something.
+    };
 };
 
 var stop = function() {
-	clearInterval(interval_id);
-	interval_id = null;
+    clearInterval(interval_id);
+    interval_id = null;
 };
 
 var reset = function() {
-	display_time = 0;
-	curr_lap_time = 0;
-	update_display();
-	laps = [];
-	clear_laps();
+    display_time = 0;
+    curr_lap_time = 0;
+    update_display();
+    laps = [];
+    clear_laps();
 };
 
 var clear_laps = function() {
-	lap_times_display.innerHTML = ""
+    lap_times_display.innerHTML = ""
 }
 
 var write_lap = function(l) {
-	if (l.length == 1) {	
-		most_recent_lap = l[0];
-	}
-	else {
-		most_recent_lap = l[l.length - 1] - l[l.length - 2];
-	}
-	lapHTML = "<div id='lap'>Lap " + l.length +": " + format_time(most_recent_lap) +"</div>"
-	lap_times_display.innerHTML += lapHTML
+    if (l.length == 1) {    
+        most_recent_lap = l[0];
+    }
+    else {
+        most_recent_lap = l[l.length - 1] - l[l.length - 2];
+    }
+    lapHTML = "<div id='lap'>Lap " + l.length +": " + format_time(most_recent_lap) +"</div>"
+    lap_times_display.innerHTML += lapHTML
 }
 
 var lap = function() {
-	// TODO- implement laps when clock stopped?
-	if (interval_id) {
-	// if clock running
-		laps.push(display_time);
-		write_lap(laps);
-		curr_lap_time = 0;
-	}
+    // TODO- implement laps when clock stopped?
+    if (interval_id) {
+    // if clock running
+        laps.push(display_time);
+        write_lap(laps);
+        curr_lap_time = 0;
+    }
 };
 
 start_button.addEventListener("click", start);
